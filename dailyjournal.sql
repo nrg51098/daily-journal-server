57c90d2d58a880bb0a226b228bb9b18ded41ac7f
Nrg51098 <gaikwadrnikhil@gmail.com>
2020-10-25 08:17:48 -0500

    finished everything except the searchEntries

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -1,14 +1,15 @@
 CREATE TABLE `JournalEntries` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`title`	TEXT NOT NULL,
-	`description`	TEXT NOT NULL,
+	`concept`	TEXT NOT NULL,
+	`entry`	TEXT NOT NULL,
+    `date` TEXT NOT NULL,
     `moods_id` INTEGER NOT NULL,
     FOREIGN KEY(`moods_id`) REFERENCES `Moods`(`id`)
 );
 
 CREATE TABLE `Moods` (
     `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `name`    TEXT NOT NULL    
+    `label`    TEXT NOT NULL    
 );
 
 INSERT INTO `Moods` VALUES (null, 'Gloomy');
@@ -19,8 +20,9 @@ INSERT INTO `Moods` VALUES (null, 'Idyllic');
 INSERT INTO `Moods` VALUES (null, 'Romantic');
 
 
-INSERT INTO `JournalEntries` VALUES (null, 'Chapter 1', 'this is chapter 1', 4);
-INSERT INTO `JournalEntries` VALUES (null, 'Chapter 2', 'this is chapter 2', 3);
-INSERT INTO `JournalEntries` VALUES (null, 'Chapter 3', 'this is chapter 3', 1);
-INSERT INTO `JournalEntries` VALUES (null, 'Chapter 4', 'this is chapter 4', 2);
-INSERT INTO `JournalEntries` VALUES (null, 'Chapter 5', 'this is chapter 5', 5);
+
+INSERT INTO `JournalEntries` VALUES (null, 'Concept 1', 'this is Entry 1', 'Jan-10-1999', 4);
+INSERT INTO `JournalEntries` VALUES (null, 'Concept 2', 'this is Entry 2', 'Jan-10-2000', 3);
+INSERT INTO `JournalEntries` VALUES (null, 'Concept 3', 'this is Entry 3', 'Jan-10-2001', 1);
+INSERT INTO `JournalEntries` VALUES (null, 'Concept 4', 'this is Entry 4', 'Jan-10-2002', 2);
+INSERT INTO `JournalEntries` VALUES (null, 'Concept 5', 'this is Entry 5', 'Jan-10-2003', 5);
